f71bca38eff53830018101c1db86570e34108ae5
Rasim Ablyalimov <rasim.a@wecandevelopit.com>
2026-01-19 20:39:53 +0300

    test autodeploy

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -92,7 +92,7 @@ export function Home() {
 				transition={{ duration: 1 }}
 				className={styles.title}
 			>
-				<span className={styles.gradientText}>Деплой проекта с нуля</span>
+				<span className={styles.gradientText}>Деплой проекта с нуля 1</span>
 			</m.h1>
 			<m.h2
 				initial={{ opacity: 0, y: 20 }}
